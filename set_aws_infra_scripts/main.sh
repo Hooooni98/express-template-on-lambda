00d8c72ff7e625cba209431d6fe1ce7a64ae6f84
Hooooni98 <piel07@naver.com>
2024-10-09 23:03:08 +0900

    feat: create aws 시 ECR_ENDPOINT env에 등록

--- set_aws_infra_scripts/main.sh
+++ set_aws_infra_scripts/main.sh
@@ -18,8 +18,8 @@ AWS_ID=$(aws sts get-caller-identity \
     --output text \
     --query 'Account')
 
-sed -i '' "s/AWS_ID=.*$/AWS_ID=$AWS_ID/" "$dir/.env.sh" # on mac
-# sed -i "s/AWS_ID=.*$/AWS_ID=$AWS_ID/" "$dir/.env.sh" # on linux
+sed -i '' "s|AWS_ID=.*$|AWS_ID=$AWS_ID|" "$dir/.env.sh" # on mac
+# sed -i "s|AWS_ID=.*$|AWS_ID=$AWS_ID|" "$dir/.env.sh" # on linux
 echo "[Success] Login to Aws"
 
 
@@ -27,6 +27,7 @@ echo "[Success] Login to Aws"
 ECR_ENDPOINT=$AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_REPOSITORY_NAME
 create_ecr $AWS_ID
 push_first_image $AWS_ID $ECR_ENDPOINT
+sed -i '' "s|ECR_ENDPOINT=.*$|ECR_ENDPOINT=$ECR_ENDPOINT|" "$dir/.env.sh"
 echo "[Success] Create ECR and Push first image"
 
 # ######## Lambda ########
